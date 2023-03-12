c2078588c6563d0eb9ddae406134693d995c743c
Alice <despero45@gmail.com>
2023-03-13 01:31:43 +1000

    second commit (sub)

--- python_project/funcs.py
+++ python_project/funcs.py
@@ -3,3 +3,9 @@ def sum_of_numbers():
     b = input("Введите второе число")
     sum_of_num = a + b
     return sum_of_num
+
+def sub_of_numbers():
+    a = input("Введите первое число")
+    b = input("Введите второе число")
+    sum_of_num = a - b
+    return sum_of_num
